5c719893df836096aca3821a4a101c9617eae41c
abhishek-goutam <abhishekgoutam8894@gmail.com>
2023-11-09 15:48:35 +0530

    login integration

--- Backend/Controller/userController.js
+++ Backend/Controller/userController.js
@@ -11,14 +11,53 @@ const registerUser = async (req, res) => {
 
   const user = await UserModel.findOne({email:email});
 
-  if(user){
-    
+  if (!user) {
+    // Hash password using bcrypt module
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new UserModel({
+      name: name,
+      email: email,
+      password: hashPassword,
+    });
+
+    const savedUser = newUser.save();
+    // create payload then Generate an access token
+
+    const token = jwt.sign({ userId: savedUser._id }, "randomsecret");
+    return res.status(200).json({
+      user: newUser,
+      token: token,
+    });
+  } else {
+    return res.status(500).send("User already exists!");
   }
-  res.send("Register User");
 };
 
 const loginUser = async (req, res) => {
-  res.send("Login User");
+  const { email, password } = req.body;
+
+  const user = await UserModel.findOne({ email: email });
+
+  if (!user) {
+    return res.status(400).send("User does not exist");
+  }
+
+  const isPasswordMatchingFromDb = await bcrypt.compare(
+    password,
+    user.password
+  );
+
+  if (isPasswordMatchingFromDb) {
+    const token = jwt.sign({ userId: user._id }, "randomsecret");
+    return res.status(200).json({
+      user: user,
+      token: token,
+    });
+  }
+
+  return res.status(401).send("Incorrect login credentials");
 };
 
 module.exports = {
